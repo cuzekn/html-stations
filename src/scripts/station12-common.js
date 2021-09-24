b3a36837533fb020a297cb1764c62f246859495d
cuzekn <86640029+cuzekn@users.noreply.github.com>
2021-09-24 22:38:01 +0900

    station12.jsの読み込み

--- src/scripts/station12-common.js
+++ src/scripts/station12-common.js
@@ -1,5 +1,3 @@
-document.write("<script src=\"scripts/station12.js\"></script>");
-
 var result = getData();
 var elem = document.getElementById("result");
 elem.innerHTML = result;
--- src/scripts/station12.js
+++ src/scripts/station12.js
@@ -1,6 +1,3 @@
-'use strict';
-{
 function getData() {
     return "Station12をクリア!"
-}
 }
