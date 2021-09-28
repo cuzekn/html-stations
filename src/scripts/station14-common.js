b92cccb7fe5611851f0bcd63f40c801ded00446a
cuzekn <86640029+cuzekn@users.noreply.github.com>
2021-09-28 12:21:59 +0900

    full_name追加

--- src/scripts/station14-common.js
+++ src/scripts/station14-common.js
@@ -1,11 +1,11 @@
 async function main() {
-    const results = await getData();
-    const elem = document.getElementById("result");
+    const results = await getData(); //testのデータが入ってる
+    const elem = document.getElementById("result"); //id:resultを取得してelemに代入
     results.forEach(result => {
-        let li = document.createElement('li');
-        let text = document.createTextNode(result.full_name);
-        li.appendChild(text);
-        elem.appendChild(li);
+        let li = document.createElement('li'); //liを作成
+        let text = document.createTextNode(result.full_name); //liの中にresult.full_nameを挿入
+        li.appendChild(text); //liの中にresulet.full_nameを追加
+        elem.appendChild(li); //ulの中にliを増やす
     })
 }
 
